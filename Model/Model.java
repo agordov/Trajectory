64251de8cb656df5444ad93db361db3681ad6dce
Dancho1 <danil_sidorov1997@mail.ru>
2017-04-24 20:32:27 +0300

    first try

--- Model/Model.java
+++ Model/Model.java
@@ -1,4 +1,23 @@
 package javaFx.Trajectory.Model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.XYChart;
+
 public class Model {
+
+    private final double FFA = 9.78;
+
+    public ObservableList<XYChart.Data> CreateTrajectory(double x0, double y0, double vx, double vy, double dt) {
+        // XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        ObservableList<XYChart.Data> data = FXCollections.observableArrayList();
+        double x = x0;
+        double y = y0;
+        while(y != 0) {
+            data.add(new XYChart.Data<Number, Number>(x, y));
+            x += vx * dt;
+            y += vy * dt + FFA * dt * dt / 2;
+        }
+        return data;
+    }
 }
